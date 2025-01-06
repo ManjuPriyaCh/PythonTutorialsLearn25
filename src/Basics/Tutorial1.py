c4cef66d124c30325944a326d13f4156a41775ed
ManjuPriyaCh <manjupriya.ch1@gmail.com>
2025-01-06 11:00:05 +0530

    Done

---
+++ src/Basics/Tutorial1.py
@@ -0,0 +1,7 @@
+# This is comment----Does not run
+#Python is a High Level Programming language
+#Python >> First do Complitation>> will get the Byte code as.pyc >> then it will Interepreted(line by line exceution)
+print("Hello")
+print("Python Learning")
+print(2+2)
+print(2-2,8*3)
